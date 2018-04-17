f733de3e67d8f900e4c6f9b3101cb6a5399b8f71
GuilhermeMot4 <32042163+GuilhermeMot4@users.noreply.github.com>
2018-04-17 17:23:02 -0300

    limop
    app

--- Limop/app/src/main/java/com/estoques/limop/limop/InsertProduto.java
+++ Limop/app/src/main/java/com/estoques/limop/limop/InsertProduto.java
@@ -4,25 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.estoques.limop.limop.CRUD.CRUD;
+import com.estoques.limop.limop.Construtoras.FornCompraConst;
+import com.estoques.limop.limop.ListView.ListViewFornCompra;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class InsertProduto extends AppCompatActivity {
+    private String idFornecedor;
 
     EditText foto,disponivel_estoque,min_estoque,max_estoque,peso_liquido,peso_bruto,nome_produto,valor_venda,valor_custo;
     Spinner fornecedor,categoriaProd;
     Button button;
+
+    ArrayAdapter<String> adapter;
+    ArrayList<FornCompraConst> fornecedores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +58,22 @@ public class InsertProduto extends AppCompatActivity {
 
 
         button                    = (Button)findViewById(R.id.button);
+
+        adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item);
+        fornecedores = new ArrayList<FornCompraConst>();
+        carregarFornecedor();
+
+        fornecedor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                idFornecedor = fornecedores.get(i).getId();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
     public void insertProduto(View v) {
         Map<String, String> params = new HashMap<String, String>();
@@ -57,6 +88,7 @@ public class InsertProduto extends AppCompatActivity {
         params.put("peso_bruto", peso_bruto.getText().toString().trim());
         params.put("valor_venda", valor_venda.getText().toString().trim());
         params.put("valor_custo", valor_custo.getText().toString().trim());
+        params.put("id_fornecedor", idFornecedor);
 
         CRUD.inserir("https://limopestoques.com.br/Android/Insert/InsertProduto.php", new Response.Listener<String>() {
             @Override
@@ -73,6 +105,48 @@ public class InsertProduto extends AppCompatActivity {
             }
         },params,getApplicationContext());
 
+    }
+    public void carregarFornecedor(){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://limopestoques.com.br/Android/Json/jsonFornecedores.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject obj = new JSONObject(response);
+
+                            JSONArray forncompraArray = obj.getJSONArray("fornecedores");
+
+                            for (int i = 0; i < forncompraArray.length(); i++){
+                                JSONObject forncompraObject = forncompraArray.getJSONObject(i);
+
+                                FornCompraConst forneCompra = new FornCompraConst(forncompraObject.getString("id_fornecedor"), forncompraObject.getString("nome_fornecedor"), forncompraObject.getString("tipo"), forncompraObject.getString("telefone_comercial"));
+
+                                fornecedores.add(forneCompra);
+                                adapter.add(forneCompra.getNome());
+                            }
+                            fornecedor.setAdapter(adapter);
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ){
+            protected Map<String, String> getParams() throws com.android.volley.AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("select", "select");
+
+                return params;
+            }
+        };
+
+        RequestQueue rq = Volley.newRequestQueue(this);
+        rq.add(stringRequest);
     }
 }
 
