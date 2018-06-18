3fb86c6d0feb4d6c047838f838f72a0e44aab06b
GuilhermeMot4 <32042163+GuilhermeMot4@users.noreply.github.com>
2018-06-18 17:20:28 -0300

    Limop
    aa

--- Limop/app/src/main/java/br/com/limopestoques/limop/InsertOS.java
+++ Limop/app/src/main/java/br/com/limopestoques/limop/InsertOS.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -60,6 +61,7 @@ public class InsertOS extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_os);
 
